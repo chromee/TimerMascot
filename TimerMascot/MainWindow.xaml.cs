11d0cf30a026a496a9221473d913851e62d4fe3c
chromee <odk.mmmm@gmail.com>
2016-10-02 19:08:36 +0900

    first commit

---
+++ TimerMascot/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace TimerMascot
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DispatcherTimer timer;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Quit_Clicked(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
+        {
+            base.OnMouseLeftButtonDown(e);
+            DragMove();
+        }
+
+        private void timerInput_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            //if()
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1.0);
+            timer.Tick += timer_Tick;
+            timer.Start();
+            int timerCount = Int32.Parse(timerInput.Text);
+            firstInputHidden();
+        }
+
+        void timer_Tick(object sender, EventArgs e)
+        {
+            //timerText.Text = ;
+        }
+
+        void firstInputHidden()
+        {
+            label.Visibility = Visibility.Hidden;
+            timerInput.Visibility = Visibility.Hidden;
+            label1.Visibility = Visibility.Hidden;
+            StartButton.Visibility = Visibility.Hidden;
+        }
+    }
+}
---
+++ TimerMascot/TimeWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TimerMascot
+{
+    /// <summary>
+    /// TimeWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class TimeWindow : Window
+    {
+        public TimeWindow()
+        {
+            InitializeComponent();
+        }
+
+        //時間を表示
+        public void DisplayTime(string Time)
+        {
+            Dispatcher.Invoke(() => this.Time.Text = Time);
+        }
+
+        //時間を消去
+        public void ClearTime()
+        {
+            DisplayTime(string.Empty);
+        }
+    }
+}
